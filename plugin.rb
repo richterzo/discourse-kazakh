237de17711f27511c72054346b40c1684f393542
Riccardo Terzaghi <87063991+skyterzaghir@users.noreply.github.com>
2022-11-10 19:54:07 +0100

    feat: added kazakh

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: discourse-kk--locale
+# about: Adds a new locale for Uzbek
+# version: 0.1
+# authors: Terzaghi Riccardo
+# url: https://github.com/richterzo/discourse-kazakh
+
+register_locale("kk", name: "Kazakh", nativeName: "ქართული", fallbackLocale: "en_GB")
+
+after_initialize do
+    if defined? DiscourseTranslator::Google::SUPPORTED_LANG_MAPPING
+      DiscourseTranslator::Google::SUPPORTED_LANG_MAPPING[:kk] = "kk"
+    end
+  end
